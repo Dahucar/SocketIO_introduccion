d561a55176edf2f271e42eb3e9df079bd0859258
Daniel Huenul <danielhuenul90@gmail.com>
2020-12-24 10:11:45 -0300

    Detalle extra

--- models/sockets.js
+++ models/sockets.js
@@ -11,6 +11,7 @@ class Socket {
             // el socket permite emitir un evento hacia el cliente
             // escuchando el evento de envio de mensaje
             socket.on('send-message', ( data ) => {
+                console.log('data from send-message: '+ data.msgContent );
                 let { msgContent } = data;
                 // DANDO UNA RESPUESTA
                 // socket: envia solo al cliente que envia la solicitud
